b1eaf02a337933659946de81761319047bd6c7c2
SilverJun <jangej1031@naver.com>
2020-08-04 16:55:21 +0900

    Fix home page

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -15,7 +15,7 @@ class HomePage extends StatelessWidget {
       body: SafeArea(
         child: ListView(
           children: [
-            SizedBox(height: 16),
+            SizedBox(height: 8),
             Container(
               height: 414,
               child: Image.asset('assets/home.jpeg',)
@@ -23,9 +23,7 @@ class HomePage extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.all(24.0),
               child: Text('''안녕하십니까? 테니스넷(tennisnet.co.kr)운영자 정진화입니다.
-정진화테니스레슨 동영상을 언제, 어디서, 누구나, 쉽게 접근하여 볼 수 있도록 테니스기술(서브, 포핸드, 백핸드, 포핸드발리, 백핸드발리, 스매쉬, 리턴, 로브, 어프로치샷)을 찾기 쉽게 정리하였습니다. 레슨동영상을 보시고 테니스코트에서 연습하여 테니스를 통해서 건강하고 행복한 삶을 영위하시기를 바랍니다.
-
-테니스넷 : http://www.tennisnet.co.kr/'''),
+정진화테니스레슨 동영상을 언제, 어디서, 누구나, 쉽게 접근하여 볼 수 있도록 테니스기술(서브, 포핸드, 백핸드, 포핸드발리, 백핸드발리, 스매쉬, 리턴, 로브, 어프로치샷)을 찾기 쉽게 정리하였습니다. 레슨동영상을 보시고 테니스코트에서 연습하여 테니스를 통해서 건강하고 행복한 삶을 영위하시기를 바랍니다.'''),
             ),
             ExpansionTile(
               initiallyExpanded: true,
@@ -38,18 +36,27 @@ class HomePage extends StatelessWidget {
               }).toList(),
             ),
             ListTile(
-              title: Text('테니스넷 이동'),
-              trailing: Icon(Icons.launch),
-              onTap: () {
-                launch('http://www.tennisnet.co.kr/');
-              },
-            ),
-            ListTile(
               title: Text("저장한 영상"),
+              trailing: Icon(Icons.navigate_next),
               onTap: () {
                 Navigator.push(context, MaterialPageRoute(builder: (context) => SavedVideoPage()));
               },
             ),
+            ListTile(
+              title: Text('정진화 테니스 아카데미 이동'),
+              trailing: Icon(Icons.launch),
+              onTap: () {
+                launch('https://blog.naver.com/jtennis2134/'); // https://www.youtube.com/channel/UC_0KlMiTzpejiet6ovAtoOQ
+              },
+            ),
+            ListTile(
+              title: Text('정진화 테니스레슨 유튜브 이동'),
+              trailing: Icon(Icons.launch),
+              onTap: () {
+                launch('https://www.youtube.com/channel/UC_0KlMiTzpejiet6ovAtoOQ');
+              },
+            ),
+
             SizedBox(height: 60.0,),
           ]
         ),
